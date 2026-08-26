25522183e5d3121dc6830d15eec811cf2e4aedd4
ZHINFINITY <2.03890437e+08+ZHINFINITY@users.noreply.github.com>
2026-08-26 23:04:31 +0000

    Add source MPV surface view base

---
+++ mpv-source/src/main/java/is/xyz/mpv/BaseMPVView.kt
@@ -0,0 +1,78 @@
+package `is`.xyz.mpv
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+/** Contains the essential surface lifecycle needed to render libmpv output. */
+abstract class BaseMPVView(context: Context, attrs: AttributeSet) :
+    SurfaceView(context, attrs), SurfaceHolder.Callback {
+
+    fun initialize(configDir: String, cacheDir: String, logLvl: String = "v", vo: String = "gpu") {
+        MPVLib.create(context, logLvl)
+        MPVLib.setOptionString("config", "yes")
+        MPVLib.setOptionString("config-dir", configDir)
+        for (option in arrayOf("gpu-shader-cache-dir", "icc-cache-dir")) {
+            MPVLib.setOptionString(option, cacheDir)
+        }
+        initOptions(vo)
+        MPVLib.init()
+        postInitOptions()
+        MPVLib.setOptionString("force-window", "no")
+        MPVLib.setOptionString("idle", "once")
+        holder.addCallback(this)
+        observeProperties()
+    }
+
+    fun destroy() {
+        holder.removeCallback(this)
+        MPVLib.destroy()
+    }
+
+    protected abstract fun initOptions(vo: String)
+    protected abstract fun postInitOptions()
+    protected abstract fun observeProperties()
+
+    private var filePath: String? = null
+
+    fun playFile(filePath: String) {
+        this.filePath = filePath
+    }
+
+    private var voInUse: String = ""
+
+    fun setVo(vo: String) {
+        voInUse = vo
+        MPVLib.setOptionString("vo", vo)
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        MPVLib.setPropertyString("android-surface-size", "${width}x$height")
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        Log.w(TAG, "attaching surface")
+        MPVLib.attachSurface(holder.surface)
+        MPVLib.setOptionString("force-window", "yes")
+        val pendingFile = filePath
+        if (pendingFile != null) {
+            MPVLib.command(arrayOf("loadfile", pendingFile))
+            filePath = null
+        } else {
+            MPVLib.setPropertyString("vo", voInUse)
+        }
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        Log.w(TAG, "detaching surface")
+        MPVLib.setPropertyString("vo", "null")
+        MPVLib.setOptionString("force-window", "no")
+        MPVLib.detachSurface()
+    }
+
+    companion object {
+        private const val TAG = "mpv"
+    }
+}
